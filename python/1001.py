a6c514db5680359bfd60d8aae02d73e1a1ca433c
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2023-04-22 00:23:39 +0600

    Create 1001.py

---
+++ python/1001.py
@@ -0,0 +1,11 @@
+# name - sayed hasan emon
+# university - green university of bangladesh
+# 2nd year student
+# platform - beecrowd
+
+inputValue_1 = input()
+inputValue_2 = input()
+
+sum = int(inputValue_1) + int(inputValue_2)
+
+print("X =", sum)
