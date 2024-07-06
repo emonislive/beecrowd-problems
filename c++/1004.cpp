3ff3f4ec588104f82e75f33f6bd160841a7765b8
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-06 22:40:48 +0600

    Create 1004.cpp

---
+++ c++/1004.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 06-07-24
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int a,b, mul;
+    cin >> a >> b;
+    mul = a * b;
+    cout << "PROD = " << mul << endl;
+    return 0;
+}
