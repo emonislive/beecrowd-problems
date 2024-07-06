60a8fcd1549343f9da94d928a30c2822d868a462
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-06 23:02:42 +0600

    Create 1008.cpp

---
+++ c++/1008.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 06-07-24
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    double c, sal;
+    cin >> a >> b >> c;
+    cout << "NUMBER = " << a << endl;
+    sal = b * c;
+    printf("SALARY = U$ %.2lf\n", sal);
+    return 0;
+}
