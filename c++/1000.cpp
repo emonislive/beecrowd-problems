297d970c8af39b53ae75db49c3581bc04c63e7c9
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-06 22:11:23 +0600

    Create 1000.cpp

---
+++ c++/1000.cpp
@@ -0,0 +1,13 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 06-07-24
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
