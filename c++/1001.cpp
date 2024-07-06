a486e99a265a57abc7bba2372c34d8bbbb07d25e
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-06 22:19:45 +0600

    Create 1001.cpp

---
+++ c++/1001.cpp
@@ -0,0 +1,16 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 06-07-24
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cin >> a >> b;
+    sum = a+b;
+    cout << "X = " << sum << endl;
+    return 0;
+}
