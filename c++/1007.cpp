10eb4439e7ebbc968c6de1bb0f3198655aa2a048
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-06 22:56:50 +0600

    Create 1007.cpp

---
+++ c++/1007.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 06-07-24
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    a = a*b;
+    b = c*d;
+    c = a - b;
+    cout << "DIFERENCA = " << c << endl;
+    return 0;
+}
