3f7dfc8218b80ef5e1361fdc0b79e5727d2dbb12
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-08 01:15:41 +0600

    Create 1014.cpp

---
+++ c++/1014.cpp
@@ -0,0 +1,18 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 08-07-24
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    double b,c;
+    cin >> a >> b;
+    c = a / b;
+    printf("%.3lf km/l\n", c);
+    return 0;
+}
