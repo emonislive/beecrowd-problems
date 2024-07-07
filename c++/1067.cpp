65cd3ed718b63e3b15d24a84b5cf11063ba01cab
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-08 01:56:16 +0600

    Create 1067.cpp

---
+++ c++/1067.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 08-07-24
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, i;
+    cin >> a;
+    for (i = 1; i <= a; i++){
+        if(i%2 != 0)
+            cout << i << endl;
+    }
+    return 0;
+}
