6948dd1bf7cff84503182bc2785a1af1dea5d88b
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-09 19:07:14 +0600

    Create 1003.cpp

---
+++ c++/1003.cpp
@@ -0,0 +1,15 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 09-07-24
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "SOMA = " << a+b << endl;
+}
