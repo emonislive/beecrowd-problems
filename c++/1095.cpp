fe988240387ad7cfb66124f0b66149f20dd2c7a2
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-09 20:24:31 +0600

    Create 1095.cpp

---
+++ c++/1095.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 09-07-24
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i = 1, j=60, k;
+    for (k = 0; k <= 12; k++){
+        printf("I=%d J=%d\n",i,j);
+        i += 3;
+        j -= 5;
+    }
+    return 0;
+}
