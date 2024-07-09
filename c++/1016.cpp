7b682e2f30b6647595fc7c7dc6be4986453b2659
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-09 18:58:02 +0600

    Create 1016.cpp

---
+++ c++/1016.cpp
@@ -0,0 +1,16 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 09-07-24
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a*2 << " minutos" << endl;
+    return 0;
+}
