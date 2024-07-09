8a95acf8f9b45e05be29a88bdccf0cc96fa88e57
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-09 19:04:42 +0600

    Create 1017.cpp

---
+++ c++/1017.cpp
@@ -0,0 +1,18 @@
+/*
+Name: Sayed Hasan Emon
+University: Green University of Bangladesh
+Subject: Computer Science & Engineering
+Year: 3rd
+Date of Submission: 09-07-24
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    double c;
+    cin >> a >> b;
+    c = (static_cast<double>(a)*b)/12;
+    printf("%.3lf\n", c);
+    return 0;
+}
