b946071341019422844b2837886f36f603e57e99
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-11-13 22:32:19 +0600

    Brute

---
+++ c++/3344.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+
+void solve(int n)
+{
+    cout << 4 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int n;
+  
+    cin >> n;
+    solve(n);
+  
+    return 0;
+}
