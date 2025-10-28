32a96ac00f74cda182d9a326f53e41ca5ae2dcac
Sayed Hasan Emon <94556687+emonislive@users.noreply.github.com>
2025-10-29 01:39:03 +0600

    Vitória and Her Indecision

---
+++ c++/1924.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    int n;
+    string s;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> s;
+
+    cout << "Ciencia da Computacao" << endl;
+    return 0;
+}
